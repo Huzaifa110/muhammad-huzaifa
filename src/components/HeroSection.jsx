6b0141c660c95d4bb52cafbe6d8fe5225a74ebaf
Huzaifa110 <huzaifazafar750@gmail.com>
2026-09-03 00:34:15 -0700

    update 2-sept-26

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -74,7 +74,7 @@ const HeroSection = ({ isNavbarOpen }) => {
                     </p>
                     <div className="flex flex-col sm:flex-row flex-wrap items-center justify-center space-y-6 sm:space-y-0 sm:space-x-6 mb-8 lg:mb-12 mx-4 px-4">
                         <a
-                            href='https://drive.google.com/uc?export=download&id=1M-kpqTOKurwfYx5YatHhhtEZy6m9Gd_G'
+                            href='https://drive.google.com/uc?export=download&id=1Nbl18gAMxAcOD8qdvaxrcQjWdPEutKYv'
                             download
                             target='_blank'
                             className="bg-gray-700 text-white hover:bg-gray-500 py-2 px-6 rounded-full text-lg lg:m-5 font-semibold transition duration-300 ease-in-out whitespace-nowrap sm:w-auto text-center"
